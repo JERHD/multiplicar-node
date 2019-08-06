c69760cb57ae4f9aa086ba77d8423aa915dffcd4
Julian Esteban Rios Holguin <jerios.developer@gmail.com>
2019-08-06 17:55:26 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+let listarTabla = (base, limite) => {
+        for (let i = 1; i <= limite; i++) {
+            // data += `${base} * ${i} = ${base * i}\n`;
+            console.log(`${base} * ${i} = ${base * i}`);
+        }
+}
+
+let crearArchivo = (base, limite) => {
+    return new Promise((resolve, reject) => {
+        if (!Number(base)) {
+            reject(`El valor introduccido ${base} no es un numero`);
+            return;
+        }
+        let data = '';
+        for (let i = 1; i <= limite; i++) {
+            data += `${base} * ${i} = ${base * i}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err) reject(err);
+            else
+                resolve(`tabla-${base}.txt`)
+        });
+    })
+}
+
+module.exports = { crearArchivo, listarTabla };
